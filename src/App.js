0d2f9469fc10720571c97fb89f6baeb708d0be49
narendra67 <mvnarendrareddy@gmail.com>
2019-07-30 01:37:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fossilDelta from "fossil-delta";
 
 function App() {
+
+  const [text, setText] = React.useState("");
+
+  const onTextChange = (e) => {
+    const newText = e.target.value
+    // console.log(text, newText)
+    const delta = fossilDelta.create(text, newText)
+    const newData = fossilDelta.apply(text, delta)
+    console.log(delta, newData.join(""))
+    const size = fossilDelta.outputSize(delta)
+    console.log("***********SIZE************")
+    console.log(size);
+    console.log("***********SIZE************")
+    setText(newText)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <textarea onChange={onTextChange} style={{width: "500px", height: "100px"}}>
+
+      </textarea>
+      
     </div>
   );
 }
